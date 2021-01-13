733a0deee1e142fbdc20269d2e9a2b96ac699820
David Dugle <david.dugleii@yahoo.com>
2021-01-12 19:09:08 -0500

    navbar is up, resume is not popping out but that's a feature for me to work on later.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
 function Navigation() {
 
     return (
 
 
-        <nav class="navbar has-background-black has-text-dark is-transparent">
-            <div class="navbar-init">
-                <a href="index.html" class="navbar-item">
-                    <p id="initials" class="py-2 px-2 is-size-1"><i class="fas fa-home"></i>
+        <nav className="navbar has-background-black has-text-dark is-transparent">
+            <div className="navbar-init">
+                <a href="index.html" className="navbar-item">
+                    <p id="initials" className="py-2 px-2 is-size-1"><i className="fas fa-home"></i>
                     </p>
                 </a>
-                {/* <a class="navbar-burger" id="burger">
+                {/* <a className="navbar-burger" id="burger">
                     <span></span>
                     <span></span>
                     <span></span>
                 </a> */}
             </div>
-            <div class="navbar-menu" id="nav-links">
-                <div class="navbar-end is-size-5">
+            <div className="navbar-menu" id="nav-links">
+                <div className="navbar-end is-size-5">
                     <a href="https://www.linkedin.com/in/david-dugle-54b16641/" rel="noreferrer" target="_blank"
-                        class="navbar-item"><i class="fab fa-linkedin"></i>
+                        className="navbar-item"><i className="fab fa-linkedin"></i>
                     </a>
-                    <a href="https://github.com/daviddugle" rel="noreferrer" target="_blank" class="navbar-item">Github</a>
-                    <a href="assets/David Dugle Resume2.1.pdf" rel="noreferrer" target="_blank" class="navbar-item">Resume</a>
+                    <a href="https://github.com/daviddugle" rel="noreferrer" target="_blank" className="navbar-item"><i className="fab fa-github"></i></a>
+                    <a href="./David Dugle Resume2.1.pdf" rel="noreferrer" target="_blank" className="navbar-item">Resume</a>
 
                 </div>
             </div>
